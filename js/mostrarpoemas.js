8ddf21250bc8b5bfbfe47a21c9b6da3afd82305c
Juan Pacheco <117188492+jspachecob@users.noreply.github.com>
2023-10-19 14:08:06 -0500

    se agrega el proyecto

---
+++ js/mostrarpoemas.js
@@ -0,0 +1,274 @@
+
+function init() {
+
+    const poemas = [
+        {
+            "titulo": "III AÑOS",
+            "cuerpo": "Te dedico todos los poemas del mundo si fuera por mi, pero ahora me quedo corto estos son de mi corazón para ti por toda la lucha que hemos vivido juntos quizá no sean perfectos, no sean lindos pero ten en cuenta que son con muchísimo amor por ser mi fan #1, por ser esa niña que me consiente y que me apoya, me siento agradecido toda la vida estaré agradecido contigo soy tu mayor admirador, no dejes de ser esa nena increíble que eres, eres un amor de persona te amo muchísimo, me siento también orgulloso por lo que hemos logrado jamás lo pensamos y simplemente fluyó todo, así es que nacen los verdaderos amores como el nuestro feliz aniversario mi vida.disfruta los poemitas 3/∞"
+        },
+        {
+            "titulo": "Cuando te conocí",
+            "cuerpo": "Cuando te conocí me sentí \n la persona más feliz del mundo \n sentí emoción cuando de ví \n mi mundo se detuvo por un segundo \n \n Te regalé fresas, tus favoritas \n tenia ganas de abrazarte... al fin \n fue una de las sensaciones mas bonitas \n fue increible te tenia a tí y tu me tenias a mi \n \n Y queria que la noche fuera eterna \n verte y escucharte hablar era lo unico que quería \n para mí fue lo mejor del mundo, que feliz me sentía \n sentí mariposas la noche entera, y al otro dia \n \n Y desde ese momento me enteré \n que eras mi media naranja \n o no sabia si alma gemela \n pero lo que si sabia es que desde ese momento te amé..."
+        },
+        {
+            "titulo": "La primera carta 💌",
+            "cuerpo": "La carta que me regalaste en esa cita \n la tengo en el cofre de mi corazoncito \n cada vez que la leo es una curita \n para mi vida, mi alma, y corazón \n \n mi emoción era tanta en ese momento \n que no pude contener el llanto \n era un llanto de de alegría \n porque por fin me querían como siempre quería \n \n yo buscaba por dentro \n algo que me diste tu desde ese momento \n amor, paz, confianza, tranquilidad, seguridad \n desde ese momento conocí la verdadera felicidad \n \n una hoja, tus manos y tu letra \n son el puente más lindo \n entre tus sentimientos y los míos \n gracias por todas esas cartas \n que me has dado a los largo de estos tres años "
+        },
+        {
+            "titulo": "1M (El primer mes)",
+            "cuerpo": "Recuerdo el primer mes emocionado \n no era cualquiera, era el primero oficial \n quien diria que ahora 3 años han pasado \n y aún sigue siendo cada mes especial \n \n Y ahora mucho mas \n porque te amo cada vez más \n sigues siendo mi princesa hermosa \n y mientras esté aquí, siempre lo serás \n \n que bonito aquel 25 de noviembre \n dónde nos agradecíamos por ser incondicionales \n que no se pierda la costumbre \n hasta el final de los finales \n \n Te dije que faltarían muchas cosas por vivir \n  no me equivoqué, cosas lindas hemos vivido \n desde un beso y un paseo por la calle y reír  \n hasta la intimidad, la felicidad y el enojo \n todo eso ha sido lo mas bonito."
+        },
+        {
+            "titulo": "Pero te conocí 🎶",
+            "cuerpo": "¿Sabes? \n nunca habia creido en los planes \n pero te conocí \n \n ¿Sabes? \n nunca había sentido por nadie \n lo que por tu sentí \n \n creía que nada saldría bien al final \n estaba tan roto y ya nadie me iba a curar \n creía que lo mio era quedarme así solo \n \n y depronto como estrella justo en medio de este mar \n me curaste el corazon, me enamoraste \n \n sin explicacion llegaste asi \n cuando el amor ya no seria para mi \n pero te conocí \n "
+        },
+        {
+            "titulo": "Sabados",
+            "cuerpo": "Los sábados son bonitos \n simplemente porque los paso a tu lado \n puedo besarte y ver tus ojitos \n solo por eso soy el más afortunado \n \n Cada sábado que estoy contigo \n mi felicidad aumenta un poco más \n y mi alma es un testigo \n de la tranquilidad que me das \n \n Quiero que todos los sábados senan junto a ti \n al igual que todos los días de la semana \n solo quiero poder hacerte feliz \n por el resto de mi vida \n \n  Gracias por tantos sábados \n llenos de amor, detalles y risas \n  incluso solo estando ahí sentados \n nunca cambiaria mis sábados por algo más"
+        },
+        {
+            "titulo": "Corazon azul 💙",
+            "cuerpo": " Lo que comenzó como una amistad \n hoy va más allá que las estrellas \n es un amor de verdad verdad \n un amor real, leal y sin fallas \n\n No sé que hubiese sido de mi sin ti \n no sé dónde estaría ahora mismo \n probablemente no tan feliz \n cayendo constantemente en aquel abismo \n\n Abismo del que tú me salvaste \n cuando me diste tu mano \n me cogiste del rostro y me besaste \n gracias por siempre, fue lo mejor del mundo \n \n Hoy sigue tu amistad y amor aquí presente \n los momentos lo pienso y soy el más feliz del mundo \n gracias a ti cambiaron muchas cosas de mi mente \n gracias a ti pude curar lo feo mi corazón "
+        },
+        {
+            "titulo": "Las recetas :3",
+            "cuerpo": "No hay nada mejor que cocinar a tu lado \n todo queda rico con nuestro toque de amor \n será de mis cosas favoritas cuando vivamos \n  y luego comer viendo tele y hacer el amor \n \n No pido mucho para ser feliz, tu a mi lado \n haciendo las cosas del día a día \n se que será lo más bonito del mundo \n y mis días estarán llenos de alegría \n \n Quiero probar tipos diferentes de pastas \n nuevas recetas que veamos \n lo vamos a lograr ya verás \n algo tan simple pero que quiero con muchas ganas \n \n Gracias por todas esas recetas juntos \n y momentos de risas en la cocina de tu casa \n los momentos de caricias cuando lavas los platos \n  será aquí y en otra vida un momento más de felicidad para mí          "
+        },
+        {
+            "titulo": "Margui 🐮",
+            "cuerpo": " Margui nació gracias a tu amor verdadero \n y por la forma como eres bonita persona \n por ser la mejor del mundo entero \n y por llegar  aportarle cosas lindas a mi vida \n\n Por esa razón es que margui está aquí \n  y duermes con ella todas las noches \n desearía poder dormir también con ustedes \n después de un beso sería el hombre más feliz \n\n Es el reflejo de esa familia que quiero tener \n junto a ti, porque eres tú la razón \n y la persona perfecta con la que quiero crecer \n eres la dueña de mi ser y mi corazón \n\n Aunque hoy no tenga mucho para darte \n discúlpame por no tener más que mi amor \n pronto tendré todo, y todo te daré \n porque meres más que el mundo entero  "
+         },
+        {
+            "titulo": 'Tulipán rosa 🌷',
+            "cuerpo": "Los dias eran normales hasta que me regalaste \n aquel tulipan que me hizo sentir alegre \n hoy por hoy adorna mi repiza \n cada vez que lo veo me saca una sonrisa \n \n Quisiera tenerlo la vida entera \n porque siento que tu amor esta dentro de el \n y si hay otra vida, conmigo estará \n así como tu en mi corazón estas \n \n Quiero que nuestro amor siga floreciendo \n como un tulipan en primavera \n bonito, radiante, grande y colorido \n así quiero nuestro amor la vida entera \n\n Está dentro de mis planes \n Regalarte tulipanes \n Aunque la vida no me va alcanzar \n Besos, amor, caricias y tulipanes tendrás... "
+        },
+        {
+            "titulo": "Primera navidad",
+            "cuerpo": "Soñaba siempre poder vivir pronto una navidad \n con una pareja, con una mujer hermosa \n con una persona increíble que me diera felicidad \n y llegaste tu, a convertirlo en una realidad \n \n Que mejor regalo que tus besos en noche buena \n fui muy feliz esa noche, llegué a casa contento \n te tenía a ti, y mi familia estaba completa \n cómo quisiera poder regresar a ese momento \n \n Doy gracias a Dios por tenerte a mi lado una navidad más \n aunque las cosas no serán iguales estamos juntos \n y no hay nada más feliz que vivir esos momentos una ves más \n abrazarte, desearte lo mejor decirte que te amo acompañado de besos \n \n Será siempre aquí y en otra vida \n lo que llena mi alma vacía \n lo que me trae calma y alegría \n en días de diciembre y navidad..."
+        },
+    ];
+
+
+    // let peroTeConoci = new  Audio('../src/music/Reik - Pero Te Conocí.mp3')
+    var titulo = document.getElementById('tituPoema');
+    var cuerpo = document.getElementById('cuerpoPoema');
+    var verVideo = document.getElementById('verVideo');
+    var repVid = document.getElementById('reproductorDeVideo');
+    var cerRep = document.getElementById('cerrarReproductor');
+    var menu = document.getElementById('menu');
+    var imagen = document.getElementById('ff');
+    var imagen2 = document.getElementById('hh');
+    var textDesc = document.getElementById('gg');
+    var p1 = document.getElementById('p1');
+    var p2 = document.getElementById('p2');
+    var p3 = document.getElementById('p3');
+    var p4 = document.getElementById('p4');
+    var p5 = document.getElementById('p5');
+    var p6 = document.getElementById('p6');
+    var p7 = document.getElementById('p7');
+    var p8 = document.getElementById('p8');
+    var p9 = document.getElementById('p9');
+    var p10 = document.getElementById('p10');
+    var p11 = document.getElementById('p11');
+
+
+    p1.innerHTML = "1.  " + poemas[0].titulo;
+    p2.innerHTML = "2.  " + poemas[1].titulo;
+    p3.innerHTML = "3.  " + poemas[2].titulo;
+    p4.innerHTML = "4.  " + poemas[3].titulo;
+    p5.innerHTML = "5.  " + poemas[4].titulo;
+    p6.innerHTML = "6.  " + poemas[5].titulo;
+    p7.innerHTML = "7.  " + poemas[6].titulo;
+    p8.innerHTML = "8.  " + poemas[7].titulo;
+    p9.innerHTML = "9.  " + poemas[8].titulo;
+    p10.innerHTML = "10. " + poemas[9].titulo;
+    p11.innerHTML = "11. " + poemas[10].titulo;
+
+
+    p1.onclick = function (e) {
+        titulo.innerText = poemas[0].titulo;
+        cuerpo.innerText = poemas[0].cuerpo;
+        imagen.style = 'width: 80%;';
+        imagen.src = "../src/img/portada.png";
+        imagen.style = 'width: 80%;';
+        imagen2.src = "";
+        verVideo.style = 'display:none;';
+        menu.style = "top: -100vh;";
+        textDesc.innerHTML = 'feliz aniversario';
+
+
+
+
+    };
+
+
+    p2.onclick = function (e) {
+        titulo.innerText = poemas[1].titulo;
+        cuerpo.innerText = poemas[1].cuerpo;
+        imagen.src = "../src/img/1.jpg";
+        imagen.style = 'width: 90%;';
+        textDesc.innerHTML = '25 de septiembre del 2020, fecha inolvidable ';
+        imagen2.src = "../src/img/3.jpg";
+        imagen2.style = 'width: 90%;';
+        verVideo.style = 'display:none;';
+        menu.style = "top: -100vh;";
+    };
+
+    p3.onclick = function (e) {
+        titulo.innerText = poemas[2].titulo;
+        cuerpo.innerText = poemas[2].cuerpo;
+        imagen.src = "../src/img/capcarta.jpg";
+        imagen.style = 'width: 90%;';
+        textDesc.innerHTML = 'Nunca me dieron una carta cuando me diste esta por eso lloré';
+        imagen2.src = "../src/img/carta.jpg";
+        imagen2.style = 'width: 90%;';
+        verVideo.style = 'display:none;';
+        menu.style = "top: -100vh;";
+
+    };
+
+    p4.onclick = function (e) {
+        titulo.innerText = poemas[3].titulo;
+        cuerpo.innerText = poemas[3].cuerpo;
+        imagen.src = "../src/img/elprimerMes.jpg";
+        imagen.style = 'width: 90%;';
+        textDesc.innerHTML = '❤️‍🩹';
+
+        imagen2.src = "";
+        verVideo.style = 'display:none;';
+        menu.style = "top: -100vh;";
+
+    };
+
+    p5.onclick = function (e) {
+        titulo.innerText = poemas[4].titulo;
+        cuerpo.innerText = poemas[4].cuerpo;
+        imagen.src = "../src/img/PTC.jpg";
+        imagen.style = 'width: 80%;';
+        textDesc.innerHTML = 'Para que nunca la olvides, nuestra cancion 🥰';
+        imagen2.src = "";
+        verVideo.style = 'display:inline;';
+        menu.style = "top: -100vh;";
+        // peroTeConoci.play(); 
+
+        verVideo.onclick = function () {
+            repVid.style = 'top:0; '
+
+        }
+
+        cerRep.onclick = function () {
+            repVid.style = 'top: -100vh;'
+        }
+
+    };
+
+    p6.onclick = function (e) {
+        titulo.innerText = poemas[5].titulo;
+        cuerpo.innerText = poemas[5].cuerpo;
+        imagen.src = "../src/img/sabados.jpg";
+        imagen.style = 'width: 90%;';
+        textDesc.innerHTML = 'Los sabados son lo mejores dias del mes';
+        imagen2.src = "";
+        verVideo.style = 'display:none;';
+        menu.style = "top: -100vh;";
+
+    };
+
+    p7.onclick = function (e) {
+        titulo.innerText = poemas[6].titulo;
+        cuerpo.innerText = poemas[6].cuerpo;
+        imagen.src = "../src/img/azul.jpg";
+        imagen.style = 'width: 90%;';
+        imagen2.src = "";
+        verVideo.style = 'display:none;';
+        menu.style = "top: -100vh;";
+        textDesc.innerHTML = '"El corazon azul es un simbolo de amor \n tambien representa la amistad" ';
+
+
+    };
+
+    p8.onclick = function (e) {
+        titulo.innerText = poemas[7].titulo;
+        cuerpo.innerText = poemas[7].cuerpo;
+        imagen.src = "../src/img/recetas.jpg";
+        imagen.style = 'width: 90%;';
+        textDesc.innerHTML = 'que lico uwu';
+
+        imagen2.src = "";
+        verVideo.style = 'display:none;';
+        menu.style = "top: -100vh;";
+
+    };
+
+    p9.onclick = function (e) {
+        titulo.innerText = poemas[8].titulo;
+        cuerpo.innerText = poemas[8].cuerpo;
+        imagen.src = "../src/img/margui.jpg";
+        imagen.style = 'width: 90%;';
+        textDesc.innerHTML = 'Te amuuuuuuuuuu';
+        imagen2.src = "../src/img/margui2.jpg";
+        imagen2.style = 'width: 90%;';
+        verVideo.style = 'display:none;';
+        menu.style = "top: -100vh;";
+
+    };
+
+    p10.onclick = function (e) {
+        titulo.innerText = poemas[9].titulo;
+        cuerpo.innerText = poemas[9].cuerpo;
+        imagen.src = "../src/img/tulipan.jpg";
+        imagen.style = 'width: 88%;';
+        textDesc.innerHTML = 'Gracias por esto 🥹';
+        imagen2.src = "";
+        verVideo.style = 'display:none;';
+        menu.style = "top: -100vh;";
+    };
+
+    p11.onclick = function (e) {
+        titulo.innerText = poemas[10].titulo;
+        cuerpo.innerText = poemas[10].cuerpo;
+        imagen.src = "../src/img/navidad.jpg";
+        imagen.style = 'width: 80%;';
+        textDesc.innerHTML = '24 - 12 - 2020';
+        imagen2.src = "";
+        verVideo.style = 'display:none;';
+        menu.style = "top: -100vh;";
+    };
+};
+
+
+
+function ingresar() {
+
+    modal = document.getElementById('modal');
+    login = document.getElementById('login');
+    bienvenida = document.getElementById('bienvenida');
+    alertaError = document.getElementById('alertaError');
+    cerrarBienvenida = document.getElementById('cerrarBienvenida');
+    DiaQueNosConocimos = document.getElementById('DiaQueNosConocimos').value;
+    DiaQueNosHicimosNovios = document.getElementById('DiaQueNosHicimosNovios').value;
+    colorFavorito = document.getElementById('colorFavorito').value;
+
+
+   
+    if (DiaQueNosConocimos == '2020-09-25' & DiaQueNosHicimosNovios == '2020-10-25' & colorFavorito == "Azul") {
+        login.style = 'display:none;';
+        bienvenida.style = 'width: 35rem; height: 25rem; font-size: 1rem;';
+
+    } else {
+        alertaError.style = 'opacity:1'
+    }
+
+    cerrarBienvenida.onclick = function () {
+        modal.style = 'top:-100vh;'
+
+    }
+
+
+}
+
+function abrirMenu() {
+    document.getElementById('menu').style = 'top:20px;'
+}
+
+
